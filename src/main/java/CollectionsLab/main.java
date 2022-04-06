8f035abf6f65f47fb2a10ec77899181710f79a07
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-04-06 10:32:48 -0400

    Initial Commit

--- src/main/java/CollectionsLab/main.java
+++ src/main/java/CollectionsLab/main.java
@@ -8,6 +8,7 @@ package CollectionsLab;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
@@ -20,39 +21,64 @@ import java.util.logging.Logger;
  */
 public class main {
 
-    public static void printItems(List<String> theList) {
-        for(String items : theList){
+    public static void printItems(Collection<String> theCollection) {
+        for (String items : theCollection) {
             System.out.println(items);
         }
     }
-    
-    public static void printIterator(List<String> theList){
+
+    public static void printIterator(Collection<String> theCollection) {
         Iterator<String> theIter;
-        theIter = theList.iterator();
-        
-        while(theIter.hasNext()){
+        theIter = theCollection.iterator();
+
+        while (theIter.hasNext()) {
             System.out.println(theIter.next());
         }
-        
     }
-    
-    
+
+    public static void removeIterator(Collection<String> theCollection, String target) {
+        Iterator<String> theIter;
+        theIter = theCollection.iterator();
+        while(theIter.hasNext()){
+            String current = theIter.next();
+            if(current.equals(target)){
+                theIter.remove();
+            }
+        }
+        System.out.println("Item not found");
+
+    }
+
     public static void main(String[] args) {
 
         try {
             File theFile = new File("C:\\Users\\soblab\\Documents\\NetBeansProjects\\CSC311GenericsLab\\input");
             Scanner theScan = new Scanner(theFile);
-            List<String> myList = new ArrayList<>();
+            Collection<String> myCollection = new ArrayList<>();
             //Added all items in file into the ArrayList
             while (theScan.hasNext()) {
-                myList.add(theScan.nextLine());
+                myCollection.add(theScan.nextLine());
             }
             //print items individually
-            printItems(myList);
+            printItems(myCollection);
             //print items with iterator
-            printIterator(myList);
+            System.out.println("");
+            printIterator(myCollection);
             //print items sequentially
-            System.out.println(myList);
+            System.out.println("");
+            System.out.println(myCollection);
+            //remove via iterator method
+            System.out.println("\nRemoving Asad");
+            removeIterator(myCollection , "Asad");
+            printIterator(myCollection);
+            System.out.println("\nRemoving Mateo");
+            removeIterator(myCollection , "Mateo");
+            printIterator(myCollection);
+            System.out.println("\nRemoving Gabriela");
+            removeIterator(myCollection , "Gabriela");
+            printIterator(myCollection);
+            
+            
         } catch (FileNotFoundException ex) {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
         }
